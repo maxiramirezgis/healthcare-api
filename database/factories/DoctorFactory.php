f47be33956742c9c98dfe4b13fd95718ad33681e
maxiramirezgis <maximo.ramirez@lightit.io>
2025-12-15 19:05:41 -0300

    feat: doctor factory

---
+++ database/factories/DoctorFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Lightit\Doctor\Domain\Models\Doctor;
+
+/**
+ * @extends Factory<Doctor>
+ */
+class DoctorFactory extends Factory
+{
+    protected $model = Doctor::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+        ];
+    }
+}
