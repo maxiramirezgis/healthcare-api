d4ccedcf222010b8af8b772a2ca369437b57da5f
maxiramirezgis <maximo.ramirez@lightit.io>
2025-12-15 16:52:28 -0300

    fix: remove silent fail

--- src/Clinic/Domain/Actions/DeleteClinicAction.php
+++ src/Clinic/Domain/Actions/DeleteClinicAction.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Lightit\Clinic\Domain\Actions;
 
 use Lightit\Clinic\Domain\Models\Clinic;
+use Throwable;
 
 class DeleteClinicAction
 {
+    /**
+     * @throws Throwable
+     */
     public function execute(Clinic $clinic): void
     {
-        $clinic->delete();
+        $clinic->deleteOrFail();
     }
 }
