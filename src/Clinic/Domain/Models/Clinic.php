df3daed8dfb433ec6502c0725e6b12e8b0f95510
maxiramirezgis <maximo.ramirez@lightit.io>
2025-12-16 14:50:33 -0300

    fix: remove nullable from property

--- src/Clinic/Domain/Models/Clinic.php
+++ src/Clinic/Domain/Models/Clinic.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Lightit\Clinic\Domain\Models;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property int                          $id
  * @property string                       $name
  * @property string                       $address
- * @property \Carbon\CarbonImmutable|null $created_at
- * @property \Carbon\CarbonImmutable|null $updated_at
+ * @property CarbonImmutable $created_at
+ * @property CarbonImmutable $updated_at
  *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Clinic newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Clinic newQuery()
