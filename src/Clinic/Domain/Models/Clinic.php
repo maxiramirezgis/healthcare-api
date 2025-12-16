4230ce622ad057ac54197ee4c3f434a30d5088a0
maxiramirezgis <maximo.ramirez@lightit.io>
2025-12-16 15:05:25 -0300

    fix: ecs fixer run

--- src/Clinic/Domain/Models/Clinic.php
+++ src/Clinic/Domain/Models/Clinic.php
@@ -8,9 +8,9 @@ use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property int                          $id
- * @property string                       $name
- * @property string                       $address
+ * @property int             $id
+ * @property string          $name
+ * @property string          $address
  * @property CarbonImmutable $created_at
  * @property CarbonImmutable $updated_at
  *
