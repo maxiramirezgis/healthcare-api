9311fb958b59a7c5113b3c1c08f980a6b24cf355
maxiramirezgis <maximo.ramirez@lightit.io>
2025-12-23 12:37:41 -0300

    feat: appointment created notification implemented

--- src/Appointments/App/Controllers/StoreAppointmentController.php
+++ src/Appointments/App/Controllers/StoreAppointmentController.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Lightit\Appointments\App\Controllers;
 
 use Dedoc\Scramble\Attributes\Group;
+use Illuminate\Container\Attributes\CurrentUser;
 use Illuminate\Http\JsonResponse;
 use Lightit\Appointments\App\Requests\UpsertAppointmentRequest;
 use Lightit\Appointments\App\Resources\AppointmentResource;
 use Lightit\Appointments\Domain\Actions\StoreAppointmentAction;
+use Lightit\Users\Domain\Models\User;
 use Throwable;
 
 #[Group('Appointments')]
@@ -20,8 +22,10 @@ final class StoreAppointmentController
     public function __invoke(
         UpsertAppointmentRequest $request,
         StoreAppointmentAction $action,
+        #[CurrentUser]
+        User $user,
     ): JsonResponse {
-        $appointment = $action->execute($request->toDto());
+        $appointment = $action->execute($request->toDto(), $user);
 
         return AppointmentResource::make($appointment)
             ->response()
